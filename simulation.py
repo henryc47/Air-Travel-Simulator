02aeb3dd2613e8f908491598d6f89c1d4c43f3c6
Henry Chadban <HenryJ.Chadban@gmail.com>
2024-01-16 16:49:33 +1100

    added in code to display stored data about airports

--- simulation.py
+++ simulation.py
@@ -6,7 +6,7 @@ import os
 class Simulation():
     #create the simulation class
     def __init__(self):
-        pass
+        self.error_logging = True
 
 
     #load all the airports from files in the airport folder
@@ -23,7 +23,7 @@ class Simulation():
         self.airport_states : list[str] = [] #states of the airports
         self.airport_countries : list[str] = [] #countries of the airports
         self.airport_unique_names : list[tuple[str,str,str]] = [] #unique name of the airport, including name, state and country
-        self.airport_name_indexs_dict : dict[tuple[str,str,str],int] = {} #dictionary allowing fast lookup of index by unique name
+        self.airport_name_indices_dict : dict[tuple[str,str,str],int] = {} #dictionary allowing fast lookup of index by unique name
         self.airport_longitudes : list[float] = [] #longitudes of the airports
         self.airport_latitudes : list[float] = [] #latitudes of the airports
 
@@ -36,12 +36,49 @@ class Simulation():
     def get_airport_names(self,df):
         num_airports = len(df)
         for i in range(num_airports):
-            name = convert_object_to_str(df.loc[i,"Name"])
-            state = convert_object_to_str(df.loc[i,"State"])
-            country = convert_object_to_str(df.loc[i,"Country"])
-            print("for i = ",i," name = ",name," state = ",state," country = ",country)
-        
-        
+            name : str = convert_object_to_str(df.loc[i,"Name"])
+            state : str = convert_object_to_str(df.loc[i,"State"])
+            country : str = convert_object_to_str(df.loc[i,"Country"])
+            unique_name : tuple[str,str,str] = (name,state,country)
+            self.airport_names.append(name)
+            self.airport_states.append(state)
+            self.airport_countries.append(country)
+            self.airport_unique_names.append(unique_name)
+            index : int = len(self.airport_names)-1
+            self.airport_name_indices_dict[unique_name] = index
+    
+    #get the index of an airport from it's unique name
+    def get_airport_index(self,unique_name) -> tuple[bool,int]:
+        index = -1
+        found_airport = False
+        if unique_name in self.airport_name_indices_dict:
+            index = self.airport_name_indices_dict[unique_name]
+            found_airport = True
+        else:
+            if self.error_logging:
+                print(unique_name, " not found in the dictionary")
+        return found_airport,index
+
+    #display an airport with unique combination of name,state and country, using each component individually
+    def display_airport_unique_name_components(self,name,state,country):
+        unique_name : tuple[str,str,str] = (name,state,country)
+        self.display_unique_name(unique_name)
+
+    #display an airport with unique combination of name,state and country, using a tuple to represent the combination
+    def display_unique_name(self,unique_name):
+        found_airport,index = self.get_airport_index(unique_name)
+        if found_airport:
+            self.display_airport_data(index)
+
+    #display the data for all airports
+    def display_all_airport_data(self):
+        num_airports = len(self.airport_names)
+        for i in range(num_airports):
+            self.display_airport_data(i)
+    
+    #display the data for a specific airport given by index
+    def display_airport_data(self,index):
+        print(self.airport_names[index],",",self.airport_states[index],",",self.airport_countries[index])
         
     
 #get the path to all files in a folder
@@ -68,3 +105,4 @@ def convert_object_to_str(object) -> str:
 if __name__ == "__main__":
     s = Simulation()
     s.load_all_airports()
+    s.display_all_airport_data()
